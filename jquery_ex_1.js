d68203ca0c5d196773a529ce636c7e5b6395ff72
Marcin Płużyński <marcinp2012@gmail.com>
2018-01-10 22:46:53 +0100

    jquery exercise no 1

---
+++ jquery_ex_1.js
@@ -0,0 +1,34 @@
+// Za pomoca wylacznie biblioteki jQuery, utworz nastepujacy kod html:
+//
+//     http://jsfiddle.net/h6gp9jnt/
+//
+// <div class="container">
+//     <div class="row">
+//        <div class="col-sm-4"><p class="description">Jeden</p></div>
+//        <div class="col-sm-4"><p class="description">Dwa</p></div>
+//        <div class="col-sm-4"><p class="description">Trzy</p></div>
+//     </div>
+//
+//     <div class="row">
+//        <div class="col-xs-6"><p class="description">Cztery</p></div>
+//        <div class="col-xs-6"><p class="description">Piec</p></div>
+//     </div>
+//
+//     <div class="row">
+//         <div class="col-md-3"><p class="description">Szesc</p></div>
+//         <div class="col-md-3"><p class="description">Siedem</p></div>
+//         <div class="col-md-3"><p class="description">Osiem</p></div>
+//     </div>
+// </div>
+//
+// Przydatne funkcje: addClass, text, append...
+//     Powodzenia!
+
+
+
+
+
+
+
+
+
