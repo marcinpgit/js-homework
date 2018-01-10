ec3f11de627187e5bc6174af112e170d3467976d
Marcin Płużyński <marcinp2012@gmail.com>
2018-01-11 00:00:01 +0100

    ready to review @pukie

--- jquery_ex_1.js
+++ jquery_ex_1.js
@@ -26,7 +26,27 @@
 
 
 
+let $container = $('<div>').addClass('container').css('background-color', 'grey');
 
+$container.prependTo('body');
+
+
+let createRow = () => $('<div>').addClass('row');
+let createCol = (size, number) => $('<div>').addClass('col-' + size + '-' + number);
+let createText = (string) => $('<p>').addClass('description').text(string);
+
+let $firstRow = createRow().append(createCol('sm', '4').append(createText('Jeden')),
+                    createCol('sm', '4').append(createText('Dwa')),
+                    createCol('sm', '4').append(createText('Trzy')));
+
+let $secondRow = createRow().append(createCol('xs', '6').append(createText('Cztery')),
+                    createCol('xs', '6').append(createText('Piec')));
+
+let $thirdRow = createRow().append(createCol('md', '3').append(createText('Szesc')),
+                    createCol('md', '3').append(createText('Siedem')),
+                    createCol('md', '3').append(createText('Osiem')));
+
+$container.append($firstRow, $secondRow, $thirdRow);
 
 
 
