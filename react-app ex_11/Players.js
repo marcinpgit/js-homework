89cd795789c069c0a4b9b5fcfe1ac850d7adee58
Marcin Płużyński <marcinp2012@gmail.com>
2018-02-01 02:25:36 +0100

    react app

---
+++ react-app ex_11/Players.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const players = [
+    { id: 1, name: 'Franek', level: 23, description: 'never played football' },
+    { id: 2, name: 'Bolek', level: 12, description: 'love donuts' },
+    { id: 3, name: 'Ola', level: 59, description: 'best player ever known' },
+    { id: 4, name: 'Tomek', level: 46, description: 'never played football' },
+    { id: 5, name: 'Lolek', level: 34, description: 'also love donuts' },
+    { id: 6, name: 'Marcin', level: 2, description: 'who is he' },
+    { id: 7, name: 'Asia', level: 44, description: 'faster than wind' },
+    { id: 8, name: 'Monika', level: 57, description: 'strong skills' },
+    { id: 9, name: 'Wiktoria', level: 48, description: 'fast and furious' },
+    { id: 10, name: 'Łukasz', level: 9, description: 'never seen playing' }
+];
