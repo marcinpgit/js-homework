518201c604eb5bc675923af4ab9fdc85bfe4784d
Marcin Płużyński <marcinp2012@gmail.com>
2018-02-02 01:39:57 +0100

    table changed

--- react-app ex_11/my-app/src/components/Main.js
+++ react-app ex_11/my-app/src/components/Main.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+
 import { players, tableHead } from '../data/Players';
+
 
 export class Main extends React.Component {
     render() {
         const playersId = Object.keys(players);
-        const tableVal = Object.values(tableHead);
         return (
         <table>
-            <th>{ tableVal[0] }</th>
-            <th>{ tableVal[1] }</th>
-            <th>{ tableVal[2] }</th>
-            <th>{ tableVal[3] }</th>
+            <th>{ tableHead[0] }</th>
+            <th>{ tableHead[1] }</th>
+            <th>{ tableHead[2] }</th>
+            <th>{ tableHead[3] }</th>
 
             { playersId.map(num =>
-                <tr>
+                <tr key={num}>
                     <td>{ players[num].id }</td>
                     <td>{ players[num].name }</td>
                     <td>{ players[num].level }</td>
