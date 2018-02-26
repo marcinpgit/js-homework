c31dbae270c27029213f3ecca1f5fda7b59a19cd
Marcin Płużyński <marcinp2012@gmail.com>
2018-02-26 12:23:20 +0100

    ready to review @pukie

--- react_app_ex_13/src/components/ContactsList.js
+++ react_app_ex_13/src/components/ContactsList.js
@@ -2,15 +2,6 @@ import React from 'react';
 
 class ContactsList extends React.Component {
 
-    // getCategory = () => {
-    //     const category = this.props.list;
-    //
-    //     return category.map(contact => contact.category.split(','))
-    //         .reduce((current, next) => [...current, ...next])
-    //         .map(cat => ` [${cat.replace(" ", "")}]`)
-    //         .filter((value, index, catArray) => catArray.indexOf(value) === index);
-    // };
-
     render() {
         const {list, removeContact} = this.props;
 
@@ -23,7 +14,7 @@ class ContactsList extends React.Component {
                     {list.map((item, id) => <li key={id}>
                             <strong>{item.name}</strong><br/>
                             {item.phone + ', ' + item.email}<br/>
-                            {/*{this.getCategory()}<br/>*/}
+                            {item.category}
                             <button onClick={() => removeContact(item.id)}>Remove Contact</button>
                         </li>
                     )}
