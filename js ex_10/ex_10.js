36b85708ca42ccd1534bff5ae79ccaaa065a0ab7
Marcin Płużyński <marcinp2012@gmail.com>
2018-04-11 19:51:11 +0200

    ex 10 js

---
+++ js ex_10/ex_10.js
@@ -0,0 +1,23 @@
+var offices = [
+    { id: "GD", name: "Gdańsk", headquarter: true },
+    { id: "GL", name: "Gliwice" },
+    { id: "KO", name: "Koszalin" }
+];
+
+var workers = [
+    { id: 1,  name: "Bartek",     type: "P", office: "GD", salary: 300 },
+    { id: 2,  name: "Wojtek",     type: "P", office: "GD", salary: 210 },
+    { id: 3,  name: "Piotr",      type: "M", office: "GL", salary: 250 },
+    { id: 4,  name: "Damian",     type: "P", office: "KO", salary: 290 },
+    { id: 5,  name: "Jan",        type: "P", office: "GL", salary: 210 },
+    { id: 6,  name: "Mateusz",    type: "P", office: "GD", salary: 290 },
+    { id: 7,  name: "Weronika",   type: "M", office: "KO", salary: 240 },
+    { id: 8,  name: "Gabriela",   type: "M", office: "GD", salary: 290 },
+    { id: 9,  name: "Alina",      type: "M", office: "KO", salary: 290 },
+    { id: 10, name: "Aleksander", type: "P", office: "GL", salary: 260 },
+    { id: 11, name: "Tomek",      type: "P", office: "GD", salary: 200 },
+    { id: 12, name: "Krzysztof",  type: "M", office: "KO", salary: 290 },
+    { id: 13, name: "Marcin",     type: "P", office: "GD", salary: 280 },
+    { id: 14, name: "Agata",      type: "P", office: "GD", salary: 230 },
+    { id: 15, name: "Magda",      type: "P", office: "KO", salary: 220 }
+];
